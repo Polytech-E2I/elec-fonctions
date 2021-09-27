70ab6e3952514a6e41272f327b9826d6a30e9e75
Robin SHAMSNEJAD <rsham@free.fr>
2021-09-27 19:30:52 +0200

    Add axis labels

--- app1_filter_tf.py
+++ app1_filter_tf.py
@@ -10,4 +10,6 @@ tf = signal.lti([1/((w0)**2),0,1], [1/((w0)**2),9.9/(w0),1])
 omega, gain, phase = tf.bode()
 
 plt.semilogx(omega, gain)
+plt.xlabel("Pulsation (rad/s)")
+plt.ylabel("Gain (dB)")
 plt.show()
