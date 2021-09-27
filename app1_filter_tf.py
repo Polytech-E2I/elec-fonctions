684e0456d9ea74592b78a6846935b5b0b5d415c0
Robin SHAMSNEJAD <rsham@free.fr>
2021-09-27 19:38:55 +0200

    Add spaces for clarity

--- app1_filter_tf.py
+++ app1_filter_tf.py
@@ -5,7 +5,7 @@ import numpy as np
 
 f0 = 100
 w0 = 2*np.pi*f0
-tf = signal.lti([1/((w0)**2),0,1], [1/((w0)**2),9.9/(w0),1])
+tf = signal.lti( [ 1/((w0)**2), 0,1 ], [ 1/((w0)**2), 9.9/(w0), 1 ] )
 
 omega, gain, phase = tf.bode()
 
