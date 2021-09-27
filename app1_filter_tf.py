00cf410629a943b6d9d79d10dff72e2729c9eb10
Robin SHAMSNEJAD <rsham@free.fr>
2021-09-27 19:38:01 +0200

    Add title

--- app1_filter_tf.py
+++ app1_filter_tf.py
@@ -10,6 +10,7 @@ tf = signal.lti([1/((w0)**2),0,1], [1/((w0)**2),9.9/(w0),1])
 omega, gain, phase = tf.bode()
 
 plt.semilogx(omega, gain)
+plt.title(r'Filtre coupe-bande $\omega_0 = 2\pi \times 100 rad.s^{-1}$')
 plt.xlabel("Pulsation (rad/s)")
 plt.ylabel("Gain (dB)")
 plt.show()
