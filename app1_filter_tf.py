139d75f7560bde1919e661a99a91237434d6dc85
Robin SHAMSNEJAD <rsham@free.fr>
2021-09-28 08:20:45 +0200

    Add phase with labels

--- app1_filter_tf.py
+++ app1_filter_tf.py
@@ -9,8 +9,22 @@ tf = signal.lti( [ 1/((w0)**2), 0,1 ], [ 1/((w0)**2), 9.9/(w0), 1 ] )
 
 omega, gain, phase = tf.bode()
 
-plt.semilogx(omega, gain)
-plt.title(r'Filtre coupe-bande $\omega_0 = 2\pi \times 100 rad.s^{-1}$')
-plt.xlabel("Pulsation (rad/s)")
-plt.ylabel("Gain (dB)")
+fig, plot_gain = plt.subplots()
+fig.suptitle(r'Filtre coupe-bande')
+
+color = 'tab:blue'
+plot_gain.semilogx(omega, gain, color=color)
+plot_gain.set_xlabel(r'Pulsation ($rad.s^{-1}$)')
+plot_gain.set_ylabel("Gain (dB)", color=color)
+plot_gain.tick_params(axis='y', labelcolor=color)
+
+plot_gain.axvline(x=w0, color='black', linestyle='--')
+plot_gain.text(w0, 2.5, r'$\omega_0 = 2\pi \times 100$', ha='center')#, transform=plot_gain.transAxes)
+
+color = 'tab:red'
+plot_phase = plot_gain.twinx()
+plot_phase.semilogx(omega, phase, color=color)
+plot_phase.set_ylabel("Phase (°)", color=color)
+plot_phase.tick_params(axis='y', labelcolor=color)
+
 plt.show()
